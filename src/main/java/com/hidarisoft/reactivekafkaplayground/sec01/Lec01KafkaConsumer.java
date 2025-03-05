f88bca21d208bc95ec2af0faa0f7fbd79dd6004d
maxpdr96 <maxpdr96@gmail.com>
2025-03-05 00:39:19 -0300

    Lec01

--- src/main/java/com/hidarisoft/reactivekafkaplayground/sec01/Lec01KafkaConsumer.java
+++ src/main/java/com/hidarisoft/reactivekafkaplayground/sec01/Lec01KafkaConsumer.java
@@ -1,4 +1,43 @@
 package com.hidarisoft.reactivekafkaplayground.sec01;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+
+import java.util.List;
+import java.util.Map;
+
+    /*
+    Goal: to demo a simple kafka consumer using reactor kafka
+     */
+
+
 public class Lec01KafkaConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec01KafkaConsumer.class);
+
+    public static void main(String[] args) {
+
+        Map<String, Object> consumerConfig = Map.<String, Object>of(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.3.25:9092,192.168.3.25:9094,192.168.3.25:9096",
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.GROUP_ID_CONFIG, "demo-group",
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+                ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1"
+        );
+
+        ReceiverOptions<Object, Object> options = ReceiverOptions.create(consumerConfig)
+                .subscription(List.of("order-events"));
+
+        KafkaReceiver.create(options)
+                .receive()
+                .doOnNext(r -> log.info("key: {}, value: {}", r.key(), r.value()))
+                .doOnNext(r -> r.receiverOffset().acknowledge())
+                .subscribe();
+
+    }
 }
